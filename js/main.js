b87795a71800818f7138861f1899f974523eed41
Yurii-Br <127565054+Yurii-Br@users.noreply.github.com>
2023-07-29 16:26:27 +0200

    fix form

--- js/main.js
+++ js/main.js
@@ -19,27 +19,7 @@ bodyScroll.addEventListener('click', (event) => {
 
 modalWindowForm.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  var formData = new FormData(myForm);
-
-  var xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        // Если ответ успешный, покажите сообщение об успешной отправке формы
-        alert("Форма успешно отправлена!");
-      } else {
-        // Если ответ не успешный, покажите сообщение об ошибке
-        alert("Ошибка при отправке формы. Попробуйте еще раз.");
-      }
-    }
-  };
-
-  xhr.open("POST", "send_email.php"); // Укажите путь к файлу обработчику на сервере (например, send_email.php)
-  xhr.send(formData);
-
-  close(); // Закрываем модальное окно после отправки формы
+  close();
   setTimeout(openThankWindow, 250);
   setTimeout(closeThankWindow, 2500);
 });
